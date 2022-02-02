c9089745d2724f59db573eb7f700ade353df4f34
Silvan <silvan.belder@wearetriple.com>
2022-02-02 21:25:20 +0100

    feat: Add footer

---
+++ src/components/footer.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  import Icon from "../components/icon.svelte";
+</script>
+
+<div class="footer">
+  <span class="text">
+    Built with Astro <Icon name="astro" />
+  </span>
+</div>
+
+<style lang="scss">
+  @use "../styles/mixins.scss" as *;
+
+  .footer {
+    background: var(--blue-500);
+    color: var(--white);
+    padding: 2.4rem;
+    text-align: center;
+  }
+
+  .text {
+    display: flex;
+    gap: 0.8rem;
+    justify-content: center;
+  }
+</style>
